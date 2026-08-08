3c63cb2ba10bd15eeafd7400975e6485ecdeeefa
BoyHeck44 <108020751+BoyHeck44@users.noreply.github.com>
2026-08-08 12:54:13 -0700

    Changed the Index Page
    Also added a User Guide button

--- frontend/app.js
+++ frontend/app.js
@@ -4,6 +4,9 @@ const newProcessButton =
 const historyButton =
     document.getElementById("historyButton");
 
+const userGuideButton =
+    document.getElementById("userGuideButton");
+
 
 newProcessButton.onclick = () => {
     window.location.href = "./start/start.html";
@@ -15,85 +18,6 @@ historyButton.onclick = () => {
 };
 
 
-const shiftCsv = document.getElementById("shiftCsv");
-const orderCsv = document.getElementById("orderCsv");
-const paymentCsv = document.getElementById("paymentCsv");
-
-
-const shiftCard = document.getElementById("shiftCard");
-const orderCard = document.getElementById("orderCard");
-const paymentCard = document.getElementById("paymentCard");
-
-const continueStep = document.getElementById("continueStep");
-
-
-function updateUploadStatus(input, card){
-
-    if(input.files.length > 0){
-
-        card.classList.add("process-complete");
-
-    } else {
-
-        card.classList.remove("process-complete");
-
-    }
-
-
-    checkAllUploads();
-
-}
-
-
-
-function checkAllUploads(){
-
-    const complete =
-        shiftCsv.files.length > 0 &&
-        orderCsv.files.length > 0 &&
-        paymentCsv.files.length > 0;
-
-
-    if(complete){
-
-        continueStep.classList.add("continue-complete");
-
-    } else {
-
-        continueStep.classList.remove("continue-complete");
-
-    }
-
-}
-
-
-
-shiftCsv.onchange = () => {
-
-    updateUploadStatus(
-        shiftCsv,
-        shiftCard
-    );
-
+userGuideButton.onclick = () => {
+    window.location.href = "./UserGuide.pdf";
 };
-
-
-orderCsv.onchange = () => {
-
-    updateUploadStatus(
-        orderCsv,
-        orderCard
-    );
-
-};
-
-
-paymentCsv.onchange = () => {
-
-    updateUploadStatus(
-        paymentCsv,
-        paymentCard
-    );
-
-};
-
